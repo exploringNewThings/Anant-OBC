e95d5ad75c51319094080ecbb8b7e86776ea4392
kush01 <kushagra.aggarwal01@gmail.com>
2017-02-07 21:30:15 +0530

    ioctl-completed

--- char_driver/hmc5883l_ioctl.h
+++ char_driver/hmc5883l_ioctl.h
@@ -1,5 +1,15 @@
 /*IOCTL parameters*/
 
-#include <asm/ioctl.h>
 #define HMC5883L_MAGIC '0xF2'
-#define HMC5883L_READ _IOR(HMC5883L_MAGIC, 1, int)
+#define HMC5883L_READ _IOR(HMC5883L_MAGIC, 1, unsigned short)
+#define HMC5883L_GET_MODE _IOR(HMC5883L_MAGIC, 2, unsigned short)
+#define HMC5883L_GET_SAMPLE _IOR(HMC5883L_MAGIC, 3, unsigned short)
+#define HMC5883L_GET_GAIN _IOR(HMC5883L_MAGIC, 4, unsigned short)
+#define HMC5883L_GET_MESURA _IOR(HMC5883L_MAGIC, 5, unsigned short)
+#define HMC5883L_GET_OUT_RATE _IOR(HMC5883L_MAGIC, 6, unsigned short)
+#define HMC5883L_SET_MODE _IOW(HMC5883L_MAGIC, 7, unsigned short)
+#define HMC5883L_SET_SAMPLE _IOW(HMC5883L_MAGIC, 8, unsigned short)
+#define HMC5883L_SET_GAIN _IOW(HMC5883L_MAGIC, 9, unsigned short)
+#define HMC5883L_SET_MESURA _IOW(HMC5883L_MAGIC, 10, unsigned short)
+#define HMC5883L_SET_OUT_RATE _IOW(HMC5883L_MAGIC, 11, unsigned short)
+
